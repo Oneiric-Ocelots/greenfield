7c3919af5bfe0f364df0e442069ad86e8a927641
Evelyn Chen <evelync93@gmail.com>
2019-10-22 15:33:08 -0400

    add react router modules and linking

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ProductDetails from "./productComponents/ProductDetails.jsx";
+import Related from "./relatedComponents/Related.jsx";
 
 class App extends React.Component {
   render() {
-    return <div>app here</div>;
+    return (
+      <Router>
+        <Link to="/someproductID">Product</Link>
+        <Switch>
+          <Route path="/someproductID">
+            {/* <App /> */}
+
+            <ProductDetails />
+            <Related />
+          </Route>
+        </Switch>
+      </Router>
+    );
+    //  <div>app here</div>;
   }
 }
 
---
+++ src/components/relatedComponents/Related.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+
+export default function Related() {
+  return <div>related here</div>;
+}
